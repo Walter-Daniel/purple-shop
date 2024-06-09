0a2518a298a8d140619fff90cc872fc7ff42bbb2
Walter Daniel <walterdcarrizo.19@gmail.com>
2024-06-09 18:34:57 -0300

    Create top menu

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,6 +1,7 @@
 import { titleFont } from '@/config/fonts'
 import Link from 'next/link'
 import React from 'react'
+import { IoCartOutline, IoSearchOutline } from 'react-icons/io5'
 
 export const TopMenu = () => {
   return (
@@ -13,7 +14,31 @@ export const TopMenu = () => {
             </Link>
         </div>
         {/* Center Menu */}
-        <div className=''></div>
+        <div className='hiden sm:block'>
+          <Link className='m-2 p-2 rounded-md transition-all hover:bg-gray-100' href='/category/men'>Men</Link>
+          <Link className='m-2 p-2 rounded-md transition-all hover:bg-gray-100' href='/category/women'>Women</Link>
+          <Link className='m-2 p-2 rounded-md transition-all hover:bg-gray-100' href='/category/kids'>Kids</Link>
+        </div>
+
+        {/* End menu */}
+        <div className='flex items-center'>
+          <Link href='/search' className='mx-2'>
+              <IoSearchOutline className='w-5 h-5'/>
+          </Link>
+          <Link href='/cart' className='mx-2'>
+            <div className='relative'>
+              <span className='absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-purple-700 text-white'>
+                3
+              </span>
+              <IoCartOutline className='w-5 h-5'/>
+            </div>
+          </Link>
+          <button
+            className='m-2 p-2 transition-all hover:bg-gray-100' 
+            >
+              Menu
+          </button>
+        </div>
     </nav>
   )
 }
