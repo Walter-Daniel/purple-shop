94d04f0c1f12eb82e99747a5e29fd64ae4a86354
Walter Daniel Carrizo <walterdcarrizo.19@gmail.com>
2024-06-12 16:02:30 -0300

    Create order detail page

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,8 +1,130 @@
+import { Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import clsx from "clsx";
+import Image from "next/image";
+import Link from "next/link";
+import { IoArrowBackCircleOutline, IoCardOutline } from "react-icons/io5";
 
-export default function OrderByIDPage() {
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2]
+]
+
+interface Props {
+  params:{
+    id: string;
+  }
+}
+
+export default function OrderByIdPage( { params }: Props ) {
+
+  const { id } = params;
+  //todo: verficated
+  //redirect
+
   return (
-    <div>
-      <h1>Order Id</h1>
+    <div className='flex justify-center items-center mb-72 px-10 sm:px-0'>
+      <div className="flex flex-col w-[1000px]">
+        <Title title={`Order #${id}`} />
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/* CHECKOUT */}
+          <div className="flex flex-col mt-5">
+            <div className={
+              clsx(
+                "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
+                {
+                  'bg-red-500': false,
+                  'bg-green-700': 'true'
+                }
+              )
+            }>
+              <IoCardOutline size={30} />
+              {/* <span className="mx-2">Payment pending</span> */}
+              <span className="mx-2">Payment Completed</span>
+            </div>
+          
+          {/* ITEMS */}
+          {
+            productsInCart.map(product => (
+              <div key={product.slug} className="flex mb-7">
+                <Image 
+                  src={`/products/${ product.images[0] }`}
+                  alt={product.title}
+                  height={ 100 }
+                  width={ 100 }
+                  style={{
+                    height: '100px',
+                    width: '100px'
+                  }}
+                  className="mr-5 rounded"
+                />
+                <div className="w-full">
+                  <p>{product.title}</p>
+                  <p>${product.price} x 3</p>
+                  <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                </div>
+              </div>
+            ))
+          }
+          </div>
+          {/* ORDER SUMMARY */}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+
+            <h2 className="text-xl mb-2 font-bold">Shipping Address</h2>
+              <div className="mb-10">
+                <p className="text-lg">Walter Carrizo</p>
+                <p>Av. Siempe viva</p>
+                <p>Col. Centro</p>
+                <p>San Miguel de Tucumán</p>
+                <p>ZC 5500</p>
+                <p>123.123.123</p>
+              </div>
+
+            {/* DIVIDER */}
+            <div className="w-full h-0.5 bg-gray-200 mb-10"/>
+
+            <h2 className="text-xl mb-2">Order Summary</h2>
+
+            <div className="grid grid-cols-2">
+              <span>Products</span>
+              <span className="text-right">3 items</span>        
+          
+              <span>Subtotal</span>
+              <span className="text-right">$100</span>
+               
+              <span>Sales tax</span>
+              <span className="text-right">$100</span>
+           
+              <span className="text-xl mt-5">Total</span>
+              <span className="text-xl mt-5 text-right">$100</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+
+              <div className={
+                clsx(
+                  "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
+                  {
+                    'bg-red-500': false,
+                    'bg-green-700': 'true'
+                  }
+                )
+              }>
+                <IoCardOutline size={30} />
+                {/* <span className="mx-2">Payment pending</span> */}
+                <span className="mx-2">Payment Completed</span>
+              </div>
+
+              <Link href='/' className="font-semibold flex justify-end items-center gap-2 text-sm">
+                <IoArrowBackCircleOutline size={30} />
+                Back to Shopping
+              </Link>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
